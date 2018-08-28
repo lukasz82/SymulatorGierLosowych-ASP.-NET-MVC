3b73a45c33cdecbb0ecc9e70b549172777201ee6
ŁukaszKa <lukasz.kazimierczak82@gmail.com>
2018-08-28 12:00:16 +0200

    dynamiczne dodawanie losów w duzym lotku /widok DuzyLotek

--- SymulatorGierLosowych.Tests/GameControllerTest.cs
+++ SymulatorGierLosowych.Tests/GameControllerTest.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace SymulatorGierLosowych.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class GameControllerTest
     {
         [TestMethod]
         public void TestMethod1()
