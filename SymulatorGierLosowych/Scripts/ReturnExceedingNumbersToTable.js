24b3c47e63f6f4c04af414bd9ab10fea3b23ba5b
ŁukaszKa <lukasz.kazimierczak82@gmail.com>
2018-09-04 11:16:53 +0200

    algorytmy js opakowane w funkcje, poprawa czytelności kodu

---
+++ SymulatorGierLosowych/Scripts/ReturnExceedingNumbersToTable.js
@@ -0,0 +1,38 @@
+﻿function ReturnExceedingNumbersToTable(table)
+{
+    console.log('tablica przekazana z funkcji' + table);
+    var max = table.length;
+    var coupon_count = 0;
+    console.log('res: ' + table);
+    console.log('max: ' + max);
+    let couponsQuantity = [];
+
+    // Zerowanie tablicy couponsQuantity
+    for (var j = 0; j <= max; j++)
+    {
+        couponsQuantity[j] = 0;
+    }
+
+    console.log('zerowanie' + couponsQuantity);
+
+    // Analizowanie tablicy w celu wyłapania ilości klikniętych 
+    for (var i = 1; i <= max; i++)
+    {
+        for (var j = 0; j < max; j++)
+        {
+            if (i == table[j])
+            {
+                console.log('i jest rownowazne table[j] : i = ' + i + 'table[j] = ' + table[j]);
+                coupon_count++;
+            }
+        }
+
+        if (coupon_count > 0)
+        {
+            couponsQuantity[i] = coupon_count;
+        }
+        coupon_count = 0;
+    }
+    console.log('coupon count !!! : ' + couponsQuantity);
+    return couponsQuantity;
+}
---
+++ SymulatorGierLosowych/Scripts/SeparationOfNumbersByATab.js
@@ -0,0 +1,33 @@
+﻿function SeparationOfNumbersByATab(table, whichTableToReturn) {
+    var numbersTab = [];
+    var couponsTab = [];
+    for (var i = 0; i < table.length; i++) {
+        var string = table[i];
+        var isTab = false;
+        var couponNumber = '';
+        var number = '';
+        for (var j = 0; j <= string.length; j++) {
+            if (string[j] == '|') {
+                couponsTab.push(couponNumber);
+                couponNumber = '';
+                isTab = true;
+                continue;
+            }
+
+            if (j == string.length) {
+                numbersTab.push(number);
+                number = '';
+                isTab = false;
+            }
+
+            if (isTab == false) couponNumber += string[j];
+            if (isTab == true) number += string[j];
+
+            //console.log('string' + string[j]);
+        }
+
+    }
+    if (whichTableToReturn == 2) return couponsTab;
+    if (whichTableToReturn == 1) return numbersTab;
+
+}
