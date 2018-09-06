b97d0c8a07b0e855d23003597473e9ad95aadaa2
ŁukaszKa <lukasz.kazimierczak82@gmail.com>
2018-09-06 12:03:54 +0200

    w miarę poprawny algorytm wpisywania liczb na kupon (nie pozwala wpisać więcej niż max)

---
+++ SymulatorGierLosowych/Scripts/ClickedCouponNumber.js
@@ -0,0 +1,12 @@
+﻿function ClickedCouponNumber(divIndex) {
+    var string = divIndex;
+    var returnString = '';
+    for (var j = 0; j <= string.length; j++) {
+        if (string[j] == '|') {
+            console.log('zwrocony string = ' + returnString);
+            return returnString;
+            
+        }
+        returnString += string[j];
+    }
+}
